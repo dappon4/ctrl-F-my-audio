c30b376969a411d6feb6f42c80da4470c3cb33de
Dahong Luo <dappon.04@gmail.com>
2024-01-27 03:18:52 -0500

    model reconstructed using easier model

--- model.py
+++ model.py
@@ -2,83 +2,39 @@ import torch
 from torch import nn
 import torchaudio
 
-def batch_norm(params):
-    def batch_norm_layer():
-        return nn.BatchNorm2d(num_features=params['num_features'], eps=params['eps'], momentum=params['momentum'], affine=params['affine'], track_running_stats=params['track_running_stats'])
-    
-    return batch_norm_layer
-
-def conv(kernl, stride, filters, params):
-    def conv_layer():
-        output = nn.Conv2d(in_channels=params['in_channels'], out_channels=filters, kernel_size=kernl, stride=stride, padding=params['padding'])
-        output = batch_norm(params)(output)
-        output = nn.ReLU()(output)
-        return output
-    return conv_layer
-
-def separable_conv(kernl, stride, filters, params):
-    def seperable_conv_layer():
-        # TODO - Add depthwise convolution
-        #output = depthwise_conv(kernl, stride, filters, params)
-        output = batch_norm(params)(output)
-        output = nn.ReLU()(output)
-        output = nn.Conv2d(in_channels=params['in_channels'], out_channels=filters, kernel_size=1, stride=1, padding=params['padding'], bias = False)
-        output = batch_norm(params)(output)
-        output = nn.ReLU()(output)
-        return output
-    return seperable_conv_layer
-
-YAMNET_LAYER_DEFS = [
-    # (name, kernel, stride, filters)
-    (conv, 3, 2, 32),
-    (separable_conv, 3, 1, 64),
-    (separable_conv, 3, 2, 128),
-    (separable_conv, 3, 1, 128),
-    (separable_conv, 3, 2, 256),
-    (separable_conv, 3, 1, 256),
-    (separable_conv, 3, 2, 512),
-    (separable_conv, 3, 1, 512),
-    (separable_conv, 3, 1, 512),
-    (separable_conv, 3, 1, 512),
-    (separable_conv, 3, 1, 512),
-    (separable_conv, 3, 1, 512),
-    (separable_conv, 3, 2, 1024),
-    (separable_conv, 3, 1, 1024),
-]
-
-class Yamnet(nn.Module):
-    def __init__(self, params):
-        super(Yamnet, self).__init__()
-        self.params = params
-        self.layers = self._build_model()
-        self._initialize_weights()
-        self.layers = []
-
-    def _build_model(self):
-        self.layers = []
-        for i, (layer_type, kernel_size, stride, filters) in enumerate(YAMNET_LAYER_DEFS):
-            params = {
-                'in_channels': self.params['in_channels'] if i == 0 else YAMNET_LAYER_DEFS[i-1][3],
-                'num_features': filters,
-                'eps': self.params['eps'],
-                'momentum': self.params['momentum'],
-                'affine': self.params['affine'],
-                'track_running_stats': self.params['track_running_stats'],
-                'padding': (kernel_size - 1) // 2
-            }
-            self.layers.append(layer_type(kernel_size, stride, filters, params))
-        return nn.Sequential(*self.layers)
-
-    def _initialize_weights(self):
-        for m in self.modules():
-            if isinstance(m, nn.Conv2d):
-                nn.init.xavier_uniform_(m.weight.data)
-                if m.bias is not None:
-                    nn.init.constant_(m.bias.data, 0.0)
-            elif isinstance(m, nn.BatchNorm2d):
-                nn.init.constant_(m.weight.data, 1.0)
-                nn.init.constant_(m.bias.data, 0.0)
-
+class AudioClassifier(nn.Module):
+    def __init__(self,output) -> None:
+        super().__init__()
+        self.conv1 = nn.Conv2d(3, 32, kernel_size=(5,5), stride=1, padding=0)
+        self.act1 = nn.ReLU()
+        self.drop1 = nn.Dropout(0.3)
+ 
+        self.conv2 = nn.Conv2d(32, 32, kernel_size=(3,3), stride=1, padding=1)
+        self.act2 = nn.ReLU()
+        self.pool2 = nn.MaxPool2d(kernel_size=(4, 4))
+ 
+        self.flat = nn.Flatten()
+ 
+        self.fc3 = nn.Linear(18432, 512)
+        self.act3 = nn.ReLU()
+        self.drop3 = nn.Dropout(0.5)
+ 
+        self.fc4 = nn.Linear(512, output)
+        
+        
     def forward(self, x):
-        x = self.layers(x)
+        # input 3x100x100, output 32x96x96
+        x = self.act1(self.conv1(x))
+        x = self.drop1(x)
+        # input 32x96x96, output 32x96x96
+        x = self.act2(self.conv2(x))
+        # input 32x96x96, output 32x24x24
+        x = self.pool2(x)
+        # input 32x24x24, output 18432
+        x = self.flat(x)
+        # input 18432, output 512
+        x = self.act3(self.fc3(x))
+        x = self.drop3(x)
+        # input 512, output 5
+        x = self.fc4(x)
         return x
--- train.py
+++ train.py
@@ -3,8 +3,21 @@ import librosa.display
 import matplotlib.pyplot as plt
 import os
 import numpy as np
+from PIL import Image
 
 # FILEPATH: /c:/Users/Dahong Luo/programming/python/ctrl-F-my-audio/test/test.ipynb
+
+def resize_img(path):
+    # Open the image
+    image = Image.open(path)
+
+    # Resize the image
+    resized_image = image.resize((100, 100))
+
+    # Save the resized image
+    resized_image.save("imgs/resized_image.png")
+
+# Call the function with the path to your image
 
 def preproces():
     filename = os.path.join("test","AUD-20231109-WA0012.mp3")
@@ -23,6 +36,7 @@ def preproces():
     plt.figure(figsize=(10, 4))
     librosa.display.specshow(spectrogram_db, sr=sr)
     plt.savefig("imgs/test.png", bbox_inches='tight', pad_inches=0)
+    resize_img("imgs/test.png")
     plt.show()
     
 
