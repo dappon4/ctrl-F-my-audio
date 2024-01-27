469aecc4b952631b5c23a381036abfe7c273d300
Dahong Luo <dappon.04@gmail.com>
2024-01-27 12:41:38 -0500

    modeling wip

--- model.py
+++ model.py
@@ -5,36 +5,59 @@ import torchaudio
 class AudioClassifier(nn.Module):
     def __init__(self,output) -> None:
         super().__init__()
-        self.conv1 = nn.Conv2d(3, 32, kernel_size=(5,5), stride=1, padding=0)
+        self.conv1 = nn.Conv2d(3, 32, kernel_size=(10,10), stride=2, padding=0)
+        self.norm1 = nn.BatchNorm2d(32)
         self.act1 = nn.ReLU()
-        self.drop1 = nn.Dropout(0.3)
+        self.drop1 = nn.Dropout(0.4)
  
-        self.conv2 = nn.Conv2d(32, 32, kernel_size=(3,3), stride=1, padding=1)
+        self.conv2 = nn.Conv2d(32, 32, kernel_size=(4,4), stride=1, padding=1)
+        self.norm2 = nn.BatchNorm2d(32)
         self.act2 = nn.ReLU()
         self.pool2 = nn.MaxPool2d(kernel_size=(4, 4))
- 
+
+        self.conv3 = nn.Conv2d(32, 64, kernel_size=(2,2), stride=1, padding=0)
+        self.norm3 = nn.BatchNorm2d(64)
+        self.act3 = nn.ReLU()
+        self.drop3 = nn.Dropout(0.4)
+        
         self.flat = nn.Flatten(start_dim=1)
  
-        self.fc3 = nn.Linear(18432, 512)
-        self.act3 = nn.ReLU()
-        self.drop3 = nn.Dropout(0.5)
- 
-        self.fc4 = nn.Linear(512, output)
+        self.fc4 = nn.Linear(30976, 512)
+        self.act4 = nn.ReLU()
+        self.drop4 = nn.Dropout(0.6)
+        
+        self.fc5 = nn.Linear(512, output)
         
         
     def forward(self, x):
-        # input 3x100x100, output 32x96x96
-        x = self.act1(self.conv1(x))
+        # fofmula: (W−K+2P)/S+1
+        
+        # input 3x200x200, output 32x96x96
+        x = self.conv1(x)
+        x = self.norm1(x)
+        x = self.act1(x)
         x = self.drop1(x)
-        # input 32x96x96, output 32x96x96
-        x = self.act2(self.conv2(x))
-        # input 32x96x96, output 32x24x24
+        # input 32x96x96, output 64x48x48
+        x = self.conv2(x)
+        x = self.norm2(x)
+        x = self.act2(x)
+        # input 64x48x48, output 64x24x24
         x = self.pool2(x)
-        # input 32x24x24, output 32x576
-        x = self.flat(x)
-        # input 18432, output 512
-        x = self.act3(self.fc3(x))
+        
+        # input 64x24x24, output 128x12x12
+        x = self.conv3(x)
+        x = self.norm3(x)
+        x = self.act3(x)
         x = self.drop3(x)
-        # input 512, output 5
+        
+        
+        # input 128x22x22, output 18432
+        x = self.flat(x)
+        
+        # input 18432, output 512
         x = self.fc4(x)
+        x = self.act4(x)
+        x = self.drop4(x)
+        # input 512, output 5
+        x = self.fc5(x)
         return x
--- train.py
+++ train.py
@@ -6,7 +6,9 @@ import numpy as np
 from PIL import Image
 import torch
 import torch.nn as nn
+import torch.optim as optim
 from torchvision import transforms
+import time
 
 from model import AudioClassifier
 from format_dataset import format
@@ -37,15 +39,21 @@ def load_data(type_map,randomize = False):
 def test_model(model,test_loader):
     correct = 0
     total = 0
+    model.eval()
+    loss = 0
+    
+    t1 = time.time()
     with torch.no_grad():
         for images, labels in test_loader:
             y_pred = model(images)
             _, predicted = torch.max(y_pred.data, 1)
             total += labels.size(0)
             correct += (predicted == labels).sum().item()
-    print(f"Accuracy of the network on the {total} test images: {100 * correct / total}%")
+    
+    t2 = time.time()
+    print(f"Accuracy of the network on the {total} test images: {100 * correct / total}%, took {t2-t1} seconds")
 
-def main(data,n_epochs,model,loss_fn,optimizer,train_size,split = True):
+def main(data,n_epochs,model,loss_fn,optimizer,train_size,split = True,train_bool = True):
     if split:
         n = len(data)
         train_num = int(n*train_size)
@@ -54,9 +62,13 @@ def main(data,n_epochs,model,loss_fn,optimizer,train_size,split = True):
     else:
         pass
     
-    train(train_loader,n_epochs,loss_fn,optimizer)
+    if train_bool:
+        t1 = time.time()
+        train(train_loader,n_epochs,loss_fn,optimizer)
+        t2 = time.time()
+        print(f"Training took {t2-t1} seconds")
+        torch.save(model.state_dict(), "models/model_4.pth")
     
-    torch.save(model.state_dict(), "models/model_3.pth")
     test_model(model,test_loader)
     test_model(model,train_loader)
     
@@ -64,21 +76,38 @@ def main(data,n_epochs,model,loss_fn,optimizer,train_size,split = True):
 def train(train_loader,epochs,loss_fn,optimizer):
     for i in range(epochs):
         print(f"Epoch {i+1}")
+        t1 = time.time()
         for inputs, labels in train_loader:
             y_pred = model(inputs)
             loss = loss_fn(y_pred, labels)
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
+        
+        t2 = time.time()
+        print(f"Epoch {i + 1} took {t2-t1} seconds")
+
+def create_dict():
+    res = {}
+    names = os.listdir("datasets/imgs")
+    names.remove("tmp")
+    for i,name in enumerate(names):
+        res[name] = i
+    
+    return res
 
 if __name__ == "__main__":
-    key_map = {"angry_sound":0,"Bird":1,"Cats":2,"Dog":3,"guns":4}
-    batch_size = 16
-    model = AudioClassifier(5)
+    key_map = create_dict()
+    batch_size = 64
+    model = AudioClassifier(len(key_map))
     loss_fn = nn.CrossEntropyLoss()
-    optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
-    n_epochs = 7
+    optimizer = optim.Adam(model.parameters(), lr=0.001)
+    n_epochs = 15
+    
+    t1 = time.time()
     data = load_data(key_map)
+    t2 = time.time()
+    print(f"Loading data took {t2-t1} seconds")
     
     main(data,n_epochs,model,loss_fn,optimizer,0.8)
     
