7cd5f2cfecfbd7c257370dc1d8eccc7bc84514cc
Dahong Luo <dappon.04@gmail.com>
2024-01-27 13:14:48 -0500

    trying out smaller model

--- train.py
+++ train.py
@@ -7,18 +7,22 @@ from PIL import Image
 import torch
 import torch.nn as nn
 from torchvision import transforms
+import time
 
 from model import AudioClassifier
 from format_dataset import format
 
-def load_data(type_map,randomize = False):
+def load_data(type_map, randomize=False):
     res = []
     for type in type_map:
         directory = f"datasets/imgs/{type}"
         file_names = os.listdir(directory)
         for file in file_names:
             resized_image = Image.open(f"datasets/imgs/{type}/{file}")
-            
+
+            # Resize the image into half resolution
+            resized_image = resized_image.resize((100,100))
+
             resized_image = resized_image.convert("RGB")
 
             # Apply the ToTensor transformation
@@ -28,22 +32,27 @@ def load_data(type_map,randomize = False):
             target = torch.tensor(type_map[type])
             # Print the PyTorch matrix
             res.append((image_tensor, target))
-    
+
     if randomize:
         np.random.shuffle(res)
-    
+
     return res
 
 def test_model(model,test_loader):
     correct = 0
     total = 0
+    model.eval()
+    
+    t1 = time.time()
     with torch.no_grad():
         for images, labels in test_loader:
             y_pred = model(images)
             _, predicted = torch.max(y_pred.data, 1)
             total += labels.size(0)
             correct += (predicted == labels).sum().item()
-    print(f"Accuracy of the network on the {total} test images: {100 * correct / total}%")
+    
+    t2 = time.time()
+    print(f"Accuracy of the network on the {total} test images: {100 * correct / total}%, time taken: {t2-t1}")
 
 def main(data,n_epochs,model,loss_fn,optimizer,train_size,split = True):
     if split:
@@ -57,6 +66,7 @@ def main(data,n_epochs,model,loss_fn,optimizer,train_size,split = True):
     train(train_loader,n_epochs,loss_fn,optimizer)
     
     torch.save(model.state_dict(), "models/model_3.pth")
+    
     test_model(model,test_loader)
     test_model(model,train_loader)
     
@@ -64,6 +74,7 @@ def main(data,n_epochs,model,loss_fn,optimizer,train_size,split = True):
 def train(train_loader,epochs,loss_fn,optimizer):
     for i in range(epochs):
         print(f"Epoch {i+1}")
+        t1 = time.time()
         for inputs, labels in train_loader:
             y_pred = model(inputs)
             loss = loss_fn(y_pred, labels)
@@ -71,14 +82,27 @@ def train(train_loader,epochs,loss_fn,optimizer):
             loss.backward()
             optimizer.step()
 
+        t2 = time.time()
+        print(f"Time taken: {t2-t1}")
+        
+def create_dict():
+    res = {}
+    names = os.listdir("datasets/imgs")
+    names.remove("tmp")
+    for i,name in enumerate(names):
+        res[name] = i
+    
+    return res
+
 if __name__ == "__main__":
-    key_map = {"angry_sound":0,"Bird":1,"Cats":2,"Dog":3,"guns":4}
-    batch_size = 16
-    model = AudioClassifier(5)
+    key_map = create_dict()
+    batch_size = 32
+    print(len(key_map))
+    model = AudioClassifier(len(key_map))
     loss_fn = nn.CrossEntropyLoss()
-    optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
-    n_epochs = 7
-    data = load_data(key_map)
+    optimizer = torch.optim.SGD(model.parameters(), lr=0.005)
+    n_epochs = 30
+    data = load_data(key_map,True)
     
     main(data,n_epochs,model,loss_fn,optimizer,0.8)
     
